6970928e3351b16f9861285f5982cdfdbf66c6e4
Evgeniy <ternovskiya@mail.ru>
2021-04-21 16:21:56 +0400

    First commit

---
+++ homework.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+class doubleList {
+	struct node {
+		node* next;
+		node* prev;
+		int data;
+		node(node* next, node* prev, int data) : next(next), prev(prev), data(data) {}
+	};
+	node* head;
+	node* tail;
+	int size;
+public:
+
+	void MoveToBegin(node* ptr) {
+		ptr->prev->next = ptr->next;
+		ptr->next->prev = ptr->prev;
+		ptr->next = head;
+		ptr->prev = nullptr;
+		head = ptr;
+		std::cout << head << std::endl;
+		std::cout << tail << std::endl;
+	}
+
+	void Move_(node* ptr, int k) {
+		for (int i = 0; i < k; ++i) {
+			ptr->prev->next = ptr->next;
+			ptr->next->prev = ptr->prev;
+			ptr->next = ptr->next->next;
+			ptr->prev = ptr->next;
+		}
+		int i = 0;
+		while (tail->prev != ptr) {
+			tail = tail->prev;
+			++i;
+		}
+		if (i < k) {
+			ptr->prev->next = ptr->next;
+			ptr->next->prev = ptr->prev;
+			ptr->prev = tail;
+			tail = ptr;
+			ptr->next = nullptr;
+		}
+		std::cout << head << std::endl;
+		std::cout << tail << std::endl;
+	}
+
+	void _Move(node* ptr, int k) {
+		for (int i = 0; i < k; ++i) {
+			ptr->prev->next = ptr->next;
+			ptr->next->prev = ptr->prev;
+			ptr->next = ptr->prev;
+			ptr->prev = ptr->prev->prev;
+		}
+		int i = 0;
+		while (head->next != ptr) {
+			head = head->next;
+			++i;
+		}
+		if (i < k) {
+			ptr->prev->next = ptr->next;
+			ptr->next->prev = ptr->prev;
+			ptr->next = head;
+			head = ptr;
+			ptr->prev = nullptr;
+		}
+		std::cout << head << std::endl;
+		std::cout << tail << std::endl;
+	}
+};
+
+
+
---
+++ homework2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+class queue {
+	struct node {
+		int data;
+		node* next;
+	};
+	node* head;
+	node* tail;
+	int size;
+public:
+	void pop(int N, node* head, node* tail) {
+		if (size != 0) {
+			if (N < size) {
+				for (int i = 0; i < N; ++i) {
+					std::cout << head->data << std::endl;
+					node* tmp = head;
+					tmp = tmp->next;
+					delete head;
+					head = tmp;
+				}
+			}
+			else {
+				for (int i = 0; i < size; ++i) {
+					std::cout << head->data << std::endl;
+					node* tmp = head;
+					tmp = tmp->next;
+					delete head;
+					head = tmp;
+				}
+			}
+			std::cout << head << std::endl;
+			std::cout << tail << std::endl;
+		}
+		else {
+			std::cout << 'nil' << std::endl;
+			std::cout << 'nil' << std::endl;
+		}
+	}
+
+	void popElem(node* head, node* tail) {
+		while (head!= nullptr && head->data % 2 != 0) {
+			std::cout << head->data << std::endl;
+			node* tmp = head;
+			tmp = tmp->next;
+			delete head;
+			head = tmp;
+		}
+		if (head != nullptr) {
+			std::cout << head << std::endl;
+			std::cout << tail << std::endl;
+		}
+		else {
+			std::cout << 'nil' << std::endl;
+			std::cout << 'nil' << std::endl;
+		}
+	}
+};
+
+
+
